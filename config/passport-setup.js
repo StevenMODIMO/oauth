7d27c74b0010f62f96c2c24e69e41d66d6ee0cd3
Esteban <stevepetes26@gmail.com>
2023-02-15 09:02:57 +0300

    Last Commit

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,42 +1,50 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./keys');
-const User = require('../models/user-model');
+require("dotenv").config();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const User = require("../models/user-model");
 
+// Serailize User
 passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
+  done(null, user.id)
+})
 
+// Deserialize User
 passport.deserializeUser((id, done) => {
-    User.findById(id).then((user) => {
-        done(null, user);
-    });
-});
+  User.findById(id).then((user) => {
+    done(null, user)
+  })
+})
 
+// strategy to use and and a callback   function
 passport.use(
-    new GoogleStrategy({
-        // options for google strategy
-        clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret,
-        callbackURL: '/auth/google/redirect'
-    }, (accessToken, refreshToken, profile, done) => {
-        // check if user already exists in our own db
-        User.findOne({googleId: profile.id}).then((currentUser) => {
-            if(currentUser){
-                // already have this user
-                console.log('user is: ', currentUser);
-                done(null, currentUser);
-            } else {
-                // if not, create user in our db
-                new User({
-                    googleId: profile.id,
-                    username: profile.displayName,
-                    thumbnail: profile._json.image.url
-                }).save().then((newUser) => {
-                    console.log('created new user: ', newUser);
-                    done(null, newUser);
-                });
-            }
-        });
-    })
+  new GoogleStrategy(
+    {
+      // options for the strategy
+      callbackURL: "/auth/google/redirect",
+      clientID: process.env.GOOGLE_CLIENT,
+      clientSecret: process.env.GOOGLE_SECRET,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // check if user exists
+      User.findOne({ googleID: profile.id }).then((currentUser) => {
+        if (currentUser) {
+          // already have user
+          console.log("Current user is:" + currentUser);
+          done(null, currentUser)
+        } else {
+          // if not, create user in the db
+          new User({
+            username: profile.displayName,
+            googleID: profile.id,
+            thumbnail: profile._json.picture
+          })
+            .save()
+            .then((newUser) => {
+              console.log("New user created: " + newUser);
+              done(null, newUser)
+            });
+        }
+      });
+    }
+  )
 );
--- server.js
+++ server.js
@@ -1,42 +1,41 @@
-const express = require('express');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const authRoutes = require('./routes/auth-routes');
-const profileRoutes = require('./routes/profile-routes');
-const passportSetup = require('./config/passport-setup');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-
+require("dotenv").config();
+const express = require("express");
+const authRoutes = require("./routes/auth-routes");
+const profileRoutes = require("./routes/profile-routes");
+const passportSetup = require("./config/passport-setup");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const passport = require("passport");
 const app = express();
 
 // set view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-// set up session cookies
-app.use(cookieSession({
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: [keys.session.cookieKey]
-}));
+app.use(
+  session({
+    secret: process.env.COOKIE_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 
-// initialize passport
+// Initialize passport to use cookies
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-// connect to mongodb
-mongoose.connect(keys.mongodb.dbURI, () => {
-    console.log('connected to mongodb');
-});
-
 // set up routes
-app.use('/auth', authRoutes);
-app.use('/profile', profileRoutes);
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
 
 // create home route
-app.get('/', (req, res) => {
-    res.render('home', { user: req.user });
+app.get("/", (req, res) => {
+  res.render('home', { user: req.user });
 });
 
-app.listen(3000, () => {
-    console.log('app now listening for requests on port 3000');
+// database connection and server launch
+mongoose.connect(process.env.MONGO_URI, () => {
+  app.listen(process.env.PORT, () => {
+    console.log(`http://localhost:${process.env.PORT}`);
+  });
 });
